95a44a67fdad2f24ea975402ede2a106fe450aa5
Guilherme Vale <guilhermevale@MacBook-Pro-de-Guilherme.local>
2020-01-20 19:23:37 -0800

    style hamburguer icon component

--- src/components/hamburguer-icon/hamburguer-icon.component.jsx
+++ src/components/hamburguer-icon/hamburguer-icon.component.jsx
@@ -1,14 +1,33 @@
-import React from 'react'; 
+import React, {useState } from 'react'; 
 
 import './hamburguer-icon.styles.sass'
 
-const HamburguerIcon = ({toggleNav}) => (
-    <div 
-        className="hamburguer-icon"
-        onClick={toggleNav}
-    >
-        HUM
-    </div>
-);
+const HamburguerIcon = ({toggleNav}) => {
+    const [open, setOpen] = useState(false);
+
+    const onClick = () => {
+        toggleNav();
+        return setOpen(!open)
+    }
+
+    return(
+        <div 
+            className="hamburguer-icon"
+            onClick={onClick}
+        >
+            <div id="menu-toggle" className={`${open ? "open" : ""}`}>
+                <div id="hamburger">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+                <div id="cross">
+                    <span></span>
+                    <span></span>
+                </div>
+            </div>
+        </div>
+    );
+}
 
 export default HamburguerIcon; 
