beed753d45d2b78856b100f1a830d836af080ff8
Guilherme Vale <guilhermevale@MacBook-Pro-de-Guilherme.local>
2020-01-30 13:53:23 -0800

    fixed extend trip copy

--- src/pages/extend-trip/extend-trip.data.js
+++ src/pages/extend-trip/extend-trip.data.js
@@ -5,7 +5,7 @@ export const COPY = {
 ],
     copyEn: ["We recommend you to fully enjoy Brazil and travel all around during your stay. Fairly close to Carneiros, you can find Fernando de Noronha, an island archipelago in the coast of Recife famously known for its natural beauty. The Northeastern region in Brazil is also to die for, with many different destinations to explore, such as São Miguel dos Milagres, which you can easily go by car (2hour drive) after the wedding. Kenoa Resort is a stunning hotel on the beach 4 hours away by car from Carneiros. And of course, there’s always Rio de Janeiro!",
     
-    "Do it before or after our celebration, but just do it! It will be worth it. And if you need any tips, please don’t hesitate to ask us. We can help you with more details and give you a list of suggestions where to go. We are delighted that you will be joining us for a couple of days to celebrate our wedding! It’s so special to have you around! “tired” or not doing anything, we have several activities that will make your experience even better.​"]
+    "Do it before or after our celebration, but just do it! It will be worth it. And if you need any tips, please don’t hesitate to ask us. We can help you with more details and give you a list of suggestions where to go. We are delighted that you will be joining us for a couple of days to celebrate our wedding! It’s so special to have you around!​"]
 }
 
 export const HERO = {
